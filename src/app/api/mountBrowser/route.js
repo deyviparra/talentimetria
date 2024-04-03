e80fac90b03af7cc2985bd160f73158c6b3c26a1
Deyvi Parra <deyvi.pr@gmail.com>
2024-04-02 21:46:18 -0500

    Fix

--- src/app/api/mountBrowser/route.js
+++ src/app/api/mountBrowser/route.js
@@ -1,13 +1,20 @@
 import puppeteer from 'puppeteer-core'
 import chromium from '@sparticuz/chromium-min'
+import path from 'path';
 
 export async function POST(request) {
-  await puppeteer.launch({
+
+  const URL = request.nextUrl.origin
+
+  const browser = await puppeteer.launch({
     args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(`${URL}/chromium-v122.0.0-pack.tar`),
     headless: chromium.headless,
     ignoreHTTPSErrors: true,
   })
+
+  await browser.close()
+
   return Response.json({ message: 'browser mounted' })
 }
